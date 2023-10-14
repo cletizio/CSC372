8c4bac5226427ec74736d8091d3a46495d52e880
cletizio <103384285+cletizio@users.noreply.github.com>
2023-10-14 16:34:29 -0400

    Update to correct code
    Uploaded incorrect files

--- CSC372_CT_1_banking/src/Test.java
+++ CSC372_CT_1_banking/src/Test.java
@@ -1,13 +1,18 @@
 public class Test {
     public static void main(String[] args) {
         BankAccount account = new BankAccount();
-        CheckingAccount checkingAcct = new CheckingAccount();
         account.setFirstName("Tom");
         account.setLastName("Smith");
         account.setAccountID(16752);
         account.deposit(10);
         account.withdrawal(10);
         account.accountSummary();
+        System.out.println();
+
+        CheckingAccount checkingAcct = new CheckingAccount();
+        checkingAcct.setFirstName("Jack");
+        checkingAcct.setLastName("Johnson");
+        checkingAcct.setAccountID(7635);
         checkingAcct.processWithdrawal(10);
         checkingAcct.accountSummary();
     }
